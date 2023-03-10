0f04e29e43c27a54f5767100dfd08e7e1e9e2dbb
Solja Virkkunen <115994166+soljatuulia@users.noreply.github.com>
2023-03-10 09:23:06 +0200

    Delete osa4/blogilista directory

--- osa4/blogilista/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-const config = require('./utils/config');
-const logger = require('./utils/logger');
-const http = require('http');
-
-app.listen(config.PORT, () => {
-	logger.info(`Server running on port ${config.PORT}`);
-});
